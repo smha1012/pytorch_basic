9c4d32ce488889b2ba23bff1db1274e63b6e342e
seungmin <h.physics77@gmail.com>
2020-07-02 16:58:15 +0900

    sample networks

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#@Filename : __init__
+#@Date : 2020-07-02-14-33
+#@Project : anogan
+#@Author : seungmin
---
+++ model/model.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#@Filename : model
+#@Date : 2020-07-02-14-24
+#@Project : anogan
+#@Author : seungmin
+
+import torch.nn as nn
+
+class Generator(nn.Module):
+
+    def __init__(self, z_dim=20, image_size=64):
+        super(Generator, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, image_size * 8,
+                               kernel_size=4, stride=1),
+            nn.BatchNorm2d(image_size * 8),
+            nn.ReLU(inplace=True))
+
+        self.layer2 = nn.Sequential(
+            nn.ConvTranspose2d(image_size * 8, image_size * 4,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(image_size * 4),
+            nn.ReLU(inplace=True))
+
+        self.layer3 = nn.Sequential(
+            nn.ConvTranspose2d(image_size * 4, image_size * 2,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(image_size * 2),
+            nn.ReLU(inplace=True))
+
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose2d(image_size * 2, image_size,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(image_size),
+            nn.ReLU(inplace=True))
+
+        self.last = nn.Sequential(
+            nn.ConvTranspose2d(image_size, 1, kernel_size=4,
+                               stride=2, padding=1),
+            nn.Tanh())
+        # 注意：白黒画像なので出力チャネルは1つだけ
+
+    def forward(self, z):
+        out = self.layer1(z)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.last(out)
+
+        return out
+
+class Discriminator(nn.Module):
+
+    def __init__(self, z_dim=20, image_size=64):
+        super(Discriminator, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, image_size, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True))
+        # 注意：白黒画像なので入力チャネルは1つだけ
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(image_size, image_size*2, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(image_size*2, image_size*4, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(image_size*4, image_size*8, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        self.last = nn.Conv2d(image_size*8, 1, kernel_size=4, stride=1)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        feature = out  # 最後にチャネルを1つに集約する手前の情報
+        feature = feature.view(feature.size()[0], -1)  # 2次元に変換
+
+        out = self.last(out)
+
+        return out, feature
